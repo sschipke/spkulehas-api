d24e113a16cb088ab826f8eedcf45656bce463f8
Scott <swschipke@gmail.com>
2022-08-08 21:36:32 -0600

    Add vldtn to rsrvtn and remove act user frm delete

--- email/index.js
+++ email/index.js
@@ -61,7 +61,7 @@ export const sendPasswordResetEmail = async (user, url) => {
   };
 
   console.log(
-    "Sending rest email to: ",
+    "Sending reset email to: ",
     user.email,
     process.env.NODE_ENV,
     mailOptions
