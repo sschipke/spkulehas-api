9e06d4019373bf009cfb6de998af404308534687
Scott <swschipke@gmail.com>
2022-08-11 11:12:51 -0600

    Add validation for email and password updates

--- validations/reservationvalidation.js
+++ validations/reservationvalidation.js
@@ -1,6 +1,15 @@
 const moment = require("moment");
 
+const WINTER_SEASON_START_2022 = "2022-10-23";
+const WINTER_SEASON_END_2022 = "2023-05-20";
+const WINTER_SEASON_START_2023 = "2023-10-18";
+const WINTER_SEASON_END_2023 = "2024-05-20";
+const WINTER_SEASON_START_2024 = "2024-10-20";
+const WINTER_SEASON_END_2024 = "2025-05-21";
+
 export const validateReservation = (reservation) => {
+  const minDate = process.env.MINIMUM_RESERVATION_DATE;
+  const maxReservationDate = process.env.MAXIMUM_RESERVATION_DATE;
   //Note: This method will mutate/update the reservation's notes & title with trimmed values
   //TODO: Delete or add color
   delete reservation.color;
@@ -21,18 +30,73 @@ export const validateReservation = (reservation) => {
     return { error: "Submitted Notes are invalid." };
   }
   reservation.notes = trimmedNotes ? trimmedNotes : "";
-  console.log("IsValid start", moment(start).isValid())
+
   if (!moment(start).isValid()) {
-    console.log("Returning error")
     return { error: "Invalid start date" };
   }
   if (!moment(end).isValid()) {
-    console.log("Error for end");
     return { error: "Invalid end date" };
   }
   if (moment(reservation.start).isSameOrAfter(reservation.end)) {
-    console.log("comparing")
+    console.log("comparing");
     return { error: "Start date cannot be after end date." };
   }
+  if (moment(start).isBefore(minDate, "day")) {
+    return { error: "This reservation is too early." };
+  }
+  if (moment(end).isAfter(maxReservationDate)) {
+    return { error: "Reservation is too late." };
+  }
+  if (!isReservationLengthValid(start, end)) {
+    return { error: "Invalid reservation length." };
+  }
+  processReserVationDates(reservation);
   return {};
+};
+
+const isReservationLengthValid = (checkinDate, checkoutDate) => {
+  const momentCheckin = moment(checkinDate);
+  const momentCheckout = moment(checkoutDate);
+  const maxLength = isInWinter(checkinDate) ? 14 : 7;
+  const minLength = 1;
+  const reservationLength = momentCheckout.diff(momentCheckin, "days");
+  console.log({ reservationLength });
+  if (reservationLength > maxLength || reservationLength < minLength) {
+    return false;
+  }
+  return true;
+};
+
+export const isInWinter = (date) => {
+  return (
+    moment(date).isBetween(
+      WINTER_SEASON_START_2022,
+      WINTER_SEASON_END_2022,
+      "day"
+    ) ||
+    moment(date).isBetween(
+      WINTER_SEASON_START_2023,
+      WINTER_SEASON_END_2023,
+      "day"
+    )
+  );
+};
+
+const processReserVationDates = (reservation) => {
+  //If the reservation starts or ends at midnight,we update it to noon
+  const noonObject = {
+    hour: 12,
+    minute: 0,
+    second: 0,
+    millisecond: 0,
+  };
+  const { start, end } = reservation;
+  const checkInHour = moment(start).hours();
+  const checkoutHour = moment(end).hours();
+  if (checkInHour === 0 || !checkInHour) {
+    reservation.start = moment(start).set(noonObject);
+  }
+  if (checkoutHour === 0 || !checkoutHour) {
+    reservation.end = moment(end).set(noonObject);
+  }
 };
---
+++ validations/userValidation.js
@@ -0,0 +1,152 @@
+const POSSIBLE_STATUSES = ["ADMIN", "D2", "D1", "S2", "S1", "U"];
+
+const USER_ID_REGEX = new RegExp(
+  /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
+);
+
+const CITY_NAME_REGEX = new RegExp(
+  /^s*[a-zA-Z]{1}[0-9a-zA-Z][0-9a-zA-Z '-.=#/]*$/
+);
+
+const STATE_CODE_REGEX = new RegExp(
+  /^(([Aa][EeLlKkSsZzRr])|([Cc][AaOoTt])|([Dd][EeCc])|([Ff][MmLl])|([Gg][AaUu])|([Hh][Ii])|([Ii][DdLlNnAa])|([Kk][SsYy])|([Ll][Aa])|([Mm][EeHhDdAaIiNnSsOoTt])|([Nn][EeVvHhJjMmYyCcDd])|([Mm][Pp])|([Oo][HhKkRr])|([Pp][WwAaRr])|([Rr][Ii])|([Ss][CcDd])|([Tt][NnXx])|([Uu][Tt])|([Vv][TtIiAa])|([Ww][AaVvIiYy]))$/
+);
+
+const ZIP_CODE_REGEX = new RegExp(/[0-9]{5}/);
+
+// This if for the 12 digit XXX-XXX-XXXX format
+const PHONE_REGEX = new RegExp(/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/);
+
+const EMAIL_REGEX = new RegExp(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/);
+
+const VALID_PASSWORD_REGEX = new RegExp(
+  /^(?!.* )(?!.*[\s])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,20}$/
+);
+
+const MIN_PASSWORD_LENGTH = 8;
+const MAX_PASSWORD_LENGTH = 20;
+const HAS_CAPITAL_REGEX = new RegExp(/(?=.*[A-Z])/);
+const HAS_LOWERCASE_REGEX = new RegExp(/(?=.*[a-z])/);
+const HAS_NUMBER_REGEX = new RegExp(/(?=.*[0-9])/);
+const HAS_SPECIAL_CHARACTER_REGEX = new RegExp(/(?=.*[!@#$%^&*])/);
+
+const HAS_WHITE_SPACE = new RegExp(/\s/);
+
+export const validateUserProfile = (userProfile) => {
+  const { user_id, name, street, city, state, zipcode, phone } = userProfile;
+
+  if (!USER_ID_REGEX.test(user_id)) {
+    return { error: "Invalid user id." };
+  }
+
+  const trimmedName = name.trim();
+  if (!trimmedName || trimmedName.length < 2 || trimmedName.length > 30) {
+    return { error: "Invalid name." };
+  }
+  userProfile.name = trimmedName;
+
+  const trimmedStreet = street.trim();
+  if (
+    !trimmedStreet ||
+    trimmedStreet.length < 15 ||
+    trimmedStreet.length > 50
+  ) {
+    return { error: "Invalid street address." };
+  }
+  userProfile.street = trimmedStreet;
+
+  const trimmedCity = city.trim();
+  if (
+    !trimmedCity ||
+    trimmedCity.length > 20 ||
+    !CITY_NAME_REGEX.test(trimmedCity)
+  ) {
+    return { error: "Invalid city name." };
+  }
+  userProfile.city = trimmedCity;
+
+  const trimmedStateCode = state.trim();
+  if (
+    trimmedStateCode.length !== 2 ||
+    !STATE_CODE_REGEX.test(trimmedStateCode)
+  ) {
+    return { error: "Invalid state code." };
+  }
+  userProfile.state = trimmedStateCode;
+
+  const trimmedZipCode = zipcode.trim();
+  if (trimmedZipCode.length !== 5 || !ZIP_CODE_REGEX.test(trimmedZipCode)) {
+    return { error: "Invalid zip code." };
+  }
+  userProfile.zipcode = trimmedZipCode;
+
+  const trimmedPhoneNumber = phone.trim();
+  if (!trimmedPhoneNumber || !PHONE_REGEX.test(trimmedPhoneNumber)) {
+    return { error: "Invalid phone number." };
+  }
+  userProfile.phone = trimmedPhoneNumber;
+  return {};
+};
+
+export const canUserUpdate = (jwtUser, id, isPassword) => {
+  if (isPassword) {
+    return jwtUser.id === id;
+  }
+  return jwtUser.status === "ADMIN" || jwtUser.id === id;
+};
+
+export const validateStatus = (jwtUser, dbProfile, requestedProfile) => {
+  const trimmedStatus = (requestedProfile.status || "").trim();
+  if (!POSSIBLE_STATUSES.includes(trimmedStatus)) {
+    return false;
+  }
+  if (jwtUser.status !== "ADMIN" && dbProfile.status !== trimmedStatus) {
+    return false;
+  }
+
+  requestedProfile.status = trimmedStatus;
+  return true;
+};
+
+export const validateEmail = (email) => {
+  console.log("Validating email");
+  const processedEmail = email.trim().toLowerCase();
+  if (!processedEmail || !EMAIL_REGEX.test(processedEmail)) {
+    return { error: "Invalid email address." };
+  }
+  email = processedEmail;
+  return {};
+};
+
+export const validatePassword = (newPassword) => {
+  if (!newPassword) {
+    return { error: "Password is missing" };
+  }
+  if (
+    newPassword.length < MIN_PASSWORD_LENGTH ||
+    newPassword.length > MAX_PASSWORD_LENGTH
+  ) {
+    return { error: "Password must be between 8 & 20 characters" };
+  }
+  if (!HAS_CAPITAL_REGEX.test(newPassword)) {
+    return { error: "Password must contain a capital letter" };
+  }
+  if (!HAS_LOWERCASE_REGEX.test(newPassword)) {
+    return { error: "Password must contain a lower case letter" };
+  }
+  if (!HAS_NUMBER_REGEX.test(newPassword)) {
+    return { error: "Password must contain at least one number" };
+  }
+  if (!HAS_SPECIAL_CHARACTER_REGEX.test(newPassword)) {
+    return {
+      error: "Password must contain at least one of the following: !@#$%^&*",
+    };
+  }
+  if (HAS_WHITE_SPACE.test(newPassword)) {
+    return { error: "Password cannot contain white space" };
+  }
+  if (!VALID_PASSWORD_REGEX.test(newPassword)) {
+    return { error: "Invalid password" };
+  }
+  return {};
+};
