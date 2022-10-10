eb028b4e5c10ffee704792bcd990cd6ca706cbfc
Scott <swschipke@gmail.com>
2022-10-10 10:07:05 -0600

    Add seeds and script for name update

---
+++ db/seeds/staging/updateNames.js
@@ -0,0 +1,26 @@
+const updateName = async (knex, first_name, last_name, userId) => {
+  console.log({first_name}, {last_name}, {userId})
+  return knex("userprofile")
+    .where({ user_id: userId })
+    .update({ first_name, last_name })
+    .catch((error) => {
+      console.error("Error updating name: ", error)
+    })
+};
+
+exports.seed = function (knex) {
+  return knex("userprofile")
+    .columns(["name", "user_id"])
+    .then((profiles) => {
+      const profilePromises = [];
+      profiles.forEach((profile) => {
+        [firstName, lastName] = profile.name.split(" ");
+        const userId = profile.user_id;
+        profilePromises.push(updateName(knex, firstName, lastName, userId));
+      });
+      return Promise.all(profilePromises);
+    })
+    .catch((err) => {
+      throw new Error("Error populating names.", err);
+    });
+};
