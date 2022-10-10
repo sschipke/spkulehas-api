54d507c1eafc92dbc325a0aaca457124e605da63
Scott <swschipke@gmail.com>
2022-10-10 10:01:45 -0600

    Update db with login and names

--- db/migrations/20221005123450_user_login.js
+++ db/migrations/20221005123450_user_login.js
@@ -1,11 +1,23 @@
 exports.up = function (knex) {
-  return knex.schema.alterTable("user", (table) => {
-    table.timestamp("last_login", { useTz: true });
-  });
+  Promise.all([
+    knex.schema.alterTable("user", (table) => {
+      table.timestamp("last_login", { useTz: true });
+    }),
+    knex.schema.alterTable("userprofile", (table) => {
+      table.string("first_name", 100);
+      table.string("last_name", 100);
+    })
+  ]);
 };
 
 exports.down = function (knex) {
-  return knex.schema.alterTable("user", (table) => {
-    table.dropColumn("last_login");
-  });
+  return Promise.all([
+    knex.schema.alterTable("user", (table) => {
+      table.dropColumn("last_login");
+    }),
+    knex.schema.alterTable("userprofile", (table) => {
+      table.dropColumn("first_name");
+      table.dropColumn("last_name");
+    })
+  ]);
 };
