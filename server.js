6b7be75fd1986c519edb00391cd746e439da5137
Scott <swschipke@gmail.com>
2024-01-26 12:38:46 -0500

    Add hostname to listen

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import app from "./app";
 app.set("port", process.env.PORT || 8080);
 app.set("hostname", process.env.HOSTNAME || "localhost");
 
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), app.get("hostname"), () => {
   console.log(
     `${app.locals.title} is running on ${app.get("hostname")}:${app.get(
       "port"
